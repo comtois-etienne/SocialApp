227bf4de2da0899316bacf2e2cd0644ea7fb6ccd
Maxime Boisvert <Maxiimmeb@hotmail.com>
2021-06-29 21:24:18 -0400

    Alias + proxy

--- ClientUI/craco.config.js
+++ ClientUI/craco.config.js
@@ -3,11 +3,8 @@ const path = require("path");
 module.exports = {
   webpack: {
     alias: {
-      "@": path.resolve(__dirname, "src/"),
-    },
-    configure: (webpackConfig, { env, paths }) => { 
-      paths.appBuild = webpackConfig.output.path = path.resolve(`${__dirname}/static`);
-      return webpackConfig;
-  }
+      "@core": path.resolve(__dirname, "src/core"),
+      "@components": path.resolve(__dirname, "src/Components"),
+    }
   },
 };
